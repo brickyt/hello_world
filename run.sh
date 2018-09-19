9d6fa9a66308c14e1f9d065454a43d98be82d9a8
brickyt <43393476+brickyt@users.noreply.github.com>
2018-09-19 09:09:02 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 /usr/bin/curl -L "www61.myfantasyleague.com/2018/export?TYPE=transactions&L=74824&W=&JSON=0" > /root/got_tradebot-master/transactions.xml
 /usr/bin/curl -L "www61.myfantasyleague.com/2018/export?TYPE=league&L=74824&W=&JSON=0" > /root/got_tradebot-master/league.xml
 /usr/bin/curl -L "www61.myfantasyleague.com/2018/export?TYPE=rosters&L=74824&W=&JSON=0" >/root/got_tradebot-master/rosters.xml
-/usr/bin/python [INSERT ABSOLUTE PATH TO tradebot.py HERE]
+/usr/bin/python [got_tradebot/tradebot.py]
