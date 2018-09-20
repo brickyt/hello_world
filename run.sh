a6445e1b8e65b82712d4b17b3e5d9a8b380519ed
brickyt <43393476+brickyt@users.noreply.github.com>
2018-09-19 19:59:59 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/curl -L "www53.myfantasyleague.com/2018/export?TYPE=transactions&L=74824&W=&JSON=0" > /root/got_tradebot-master/transactions.xml
-/usr/bin/curl -L "www53.myfantasyleague.com/2018/export?TYPE=league&L=74824&W=&JSON=0" > /root/got_tradebot-master/league.xml
-/usr/bin/curl -L "www53.myfantasyleague.com/2018/export?TYPE=rosters&L=74824&W=&JSON=0" >/root/got_tradebot-master/rosters.xml
-/usr/bin/python [got_tradebot/tradebot.py]
